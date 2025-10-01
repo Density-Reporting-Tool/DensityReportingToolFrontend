dc26ac923f8b2df5dd030bd9333dea3bad13a8e1
izcheung <icheung24@my.bcit.ca>
2025-10-01 16:42:32 -0700

    style: add green border around selected proctor and navigate when info icon is clicked

--- src/components/card/ProctorCard.tsx
+++ src/components/card/ProctorCard.tsx
@@ -1,19 +1,29 @@
-import { Box, Card, Stack, Typography } from "@mui/material";
+import { Box, Card, IconButton, Stack, Typography } from "@mui/material";
 import { UNITS } from "@/utils/constants";
 import { ProctorData } from "@/types/proctors";
+import { InfoOutlined } from "@mui/icons-material";
 import NoPhoto from "../photo/NoPhoto";
+import { useNavigate } from "react-router-dom";
 
 interface ProctorCardType {
   index?: number;
+  selectedIndex?: number;
   proctor: ProctorData;
   handleClick: () => void;
 }
 
 const ProctorCard: React.FC<ProctorCardType> = ({
   index,
+  selectedIndex,
   proctor,
   handleClick,
 }) => {
+  const navigate = useNavigate();
+  const handleOpenDetails = () => {
+    console.log(proctor);
+    // TODO: ensure id is defined
+    navigate(`/proctors/${proctor.id}`, { state: { proctor } });
+  };
   return (
     <>
       <Card
@@ -21,42 +31,66 @@ const ProctorCard: React.FC<ProctorCardType> = ({
         sx={{
           borderRadius: "10px",
           boxShadow: "1px",
-          border: "1px lightgrey solid",
+          border:
+            selectedIndex == index ? "4px green solid" : "1px lightgrey solid",
+          padding: 0.5,
         }}
         onClick={handleClick}
       >
-        <Stack direction="row" sx={{ display: "flex", alignItems: "center" }}>
-          {proctor?.image_src ? (
-            <Box
-              component="img"
-              sx={{
-                width: "100%",
-                height: "auto",
-                maxWidth: "100px",
-                borderRadius: 2,
-                mr: 2,
-              }}
-              alt="Report photos"
-              src={proctor?.image_src || "https://placehold.co/100"}
-            />
-          ) : (
-            <NoPhoto width={100} height={100} />
-          )}
-          <Box sx={{ ml: 1 }}>
-            <Typography variant="body1" fontWeight={600}>
-              Proctor #{(index ?? 0) + 1}: {proctor?.materialType}
-            </Typography>
-            <Typography variant="body2">
-              Density: {proctor?.maxDryDensity} {UNITS.density}
-            </Typography>
-            <Typography variant="body2">
-              Corrected Density: {proctor?.correctedDensity} {UNITS.density}
-            </Typography>
-            <Typography variant="body2">
-              Optimum Moisture: {proctor?.optimumMoisture}
-              {UNITS.moisture}
-            </Typography>
+        <Stack
+          direction="row"
+          sx={{
+            justifyContent: "space-between",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            {/* Image */}
+            {proctor?.image_src ? (
+              <Box
+                component="img"
+                sx={{
+                  width: "100%",
+                  height: "auto",
+                  maxWidth: "100px",
+                  borderRadius: 2,
+                  mr: 2,
+                }}
+                alt="Report photos"
+                src={proctor?.image_src || "https://placehold.co/100"}
+              />
+            ) : (
+              <NoPhoto width={100} height={100} />
+            )}
+            {/* Proctor Details */}
+            <Box sx={{ ml: 1 }}>
+              <Typography variant="body1" fontWeight={600}>
+                Proctor #{(index ?? 0) + 1}: {proctor?.materialType}
+              </Typography>
+              <Typography variant="body2">
+                Density: {proctor?.maxDryDensity} {UNITS.density}
+              </Typography>
+              <Typography variant="body2">
+                Corrected Density: {proctor?.correctedDensity} {UNITS.density}
+              </Typography>
+              <Typography variant="body2">
+                Optimum Moisture: {proctor?.optimumMoisture}
+                {UNITS.moisture}
+              </Typography>
+            </Box>
           </Box>
+          <IconButton
+            onClick={(e) => {
+              e.stopPropagation();
+              handleOpenDetails();
+            }}
+          >
+            <InfoOutlined />
+          </IconButton>
         </Stack>
       </Card>
     </>
