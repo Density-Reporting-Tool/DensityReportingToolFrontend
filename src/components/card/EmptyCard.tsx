22e83eb36b4b787f45ba2ede004ceefee30e1927
izcheung <icheung24@my.bcit.ca>
2025-10-01 11:19:46 -0700

    style: add handleClick and style of empty card

--- src/components/card/EmptyCard.tsx
+++ src/components/card/EmptyCard.tsx
@@ -1,26 +1,28 @@
-import { Card, Typography } from "@mui/material";
+import { Box, Card, Stack, Typography } from "@mui/material";
 import { grey } from "@mui/material/colors";
 
 interface CardProps {
-  children: string;
+  text: string;
+  handleClick?: () => void;
 }
 
-const EmptyCard: React.FC<CardProps> = ({ children }) => {
-  return (
-    <Card
-      sx={{
-        padding: 2,
-        borderRadius: 2,
-        backgroundColor: grey[300],
-        display: "flex",
-        justifyContent: "center",
-        boxShadow: "none",
-      }}
-    >
-      <Typography variant="body1" color={grey[600]}>
-        {children}
-      </Typography>
-    </Card>
-  );
-};
+const EmptyCard: React.FC<CardProps> = ({ text, handleClick }) => (
+  <Card
+    sx={{
+      padding: 2,
+      borderRadius: 2,
+      backgroundColor: grey[300],
+      display: "flex",
+      justifyContent: "center",
+      boxShadow: "none",
+    }}
+    onClick={handleClick}
+  >
+    <Stack direction="row" sx={{ display: "flex", alignItems: "center" }}>
+      <Box>
+        <Typography variant="body1">{text}</Typography>
+      </Box>
+    </Stack>
+  </Card>
+);
 export default EmptyCard;
