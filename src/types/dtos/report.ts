7e55b7b27d36a74f96a080925b7d77c787fe8e01
izcheung <icheung24@my.bcit.ca>
2025-11-18 21:46:18 -0800

    fix: add report types based on returned api structure

--- src/types/dtos/report.ts
+++ src/types/dtos/report.ts
@@ -2,6 +2,7 @@ export interface ReportBaseDTO {
   jobId: number;
   employeeId: number;
   reviewerId: number;
+
   reportNumber: number;
   startDate?: string; // ISO 8601 string
   submitDate?: string; // ISO 8601 string
@@ -15,11 +16,57 @@ export interface ReportUpdateDTO extends ReportBaseDTO {}
 
 export interface ReportReadDTO extends ReportBaseDTO {
   id: number;
+
   employeeName: string;
   reviewerName: string;
-  // densityTests?: DensityTestReadDTO[];
 
   //   photos?: ReportPhotoDTO[];
   //   memos?: ReportMemoDTO[];
   //   densityTests?: DensityTestDTO[];
+}
+
+export interface PersonData {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+}
+
+export interface ReportListDTO {
+  id: number;
+  jobId: number;
+  reportNumber: number;
+  startDate?: string;
+  submitDate?: string;
+  distributeDate?: string;
+  employee: PersonData;
+  reviewer: PersonData;
+  densityTestsCount: 0;
+  photosCount: 0;
+  memosCount: 1;
+  distributionListId: null;
+}
+
+export interface ReportDetailDTO {
+  id: number;
+  jobId: number;
+  job: JobData;
+  reportNumber: number;
+  startDate?: string;
+  submitDate?: string;
+  distributeDate?: string;
+  employee: PersonData;
+  reviewer: PersonData;
+  densityTests: [];
+  photos: [];
+  memos: [];
+  distributionListId: null;
+}
+
+export interface JobData {
+  id: number;
+  jobNumber: string;
+  clientName: string;
+  projectName: string;
 }
